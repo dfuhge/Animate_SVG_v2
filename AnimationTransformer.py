be55ac31d50c046e03bfb702380947d93f3961a9
Cornelius <cor.reb@posteo.de>
2024-01-12 12:27:23 +0100

    AnimationTransformer Update

--- AnimationTransformer.py
+++ AnimationTransformer.py
@@ -1,32 +1,68 @@
+import math
+
 import torch
 import torch.nn as nn
 
 
 class AnimationTransformer(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_heads, dropout_prob):
-        super(AnimationTransformer, self).__init__()
+    def __init__(
+            self,
+            num_tokens,
+            dim_model, # hidden_size; corresponds to embedding length
+            num_heads,
+            num_encoder_layers,
+            num_decoder_layers,
+            dropout_p,
+    ):
+        super().__init__()
 
-        # Define the layers and components of the transformer
-        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.model_type = "Transformer"
+        self.dim_model = dim_model
+
+        # TODO: Currently left out, as input sequence shuffled. Later check if use is beneficial.
+        # self.positional_encoder = PositionalEncoding(dim_model=dim_model, dropout_p=dropout_p, max_len=5000)
+
         self.transformer = nn.Transformer(
-            d_model=hidden_size,
+            d_model=dim_model,
             nhead=num_heads,
-            num_encoder_layers=num_layers,
-            num_decoder_layers=num_layers,
-            dim_feedforward=hidden_size * 4,  # Adjust as needed
-            dropout=dropout_prob
+            num_encoder_layers=num_encoder_layers,
+            num_decoder_layers=num_decoder_layers,
+            dropout=dropout_p,
+            batch_first=True
         )
-        self.fc = nn.Linear(hidden_size, output_size)  # Adjust output_size as needed
-        self.softmax = nn.Softmax(dim=2)
+        self.out = nn.Linear(dim_model, num_tokens)
+        self.softmax = nn.Softmax(dim=2)  # Todo: Softmax over Categorical values
 
-    def forward(self, src, tgt):
-        # Forward pass through the transformer
-        src_embedded = self.embedding(src)
-        tgt_embedded = self.embedding(tgt)
-        transformer_output = self.transformer(src_embedded, tgt_embedded)
+    def forward(self, src, tgt, tgt_mask=None, src_pad_mask=None, tgt_pad_mask=None, batch_first=True):
+        # Src size must be (batch_size, src sequence length)
+        # Tgt size must be (batch_size, tgt sequence length)
 
-        # Apply linear layer and softmax
-        output = self.fc(transformer_output)
-        output = self.softmax(output)
+        # Transformer blocks - Out size = (sequence length, batch_size, num_tokens)
+        transformer_out = self.transformer(src, tgt, tgt_mask=tgt_mask, src_key_padding_mask=src_pad_mask,
+                                           tgt_key_padding_mask=tgt_pad_mask)
+        out = self.out(transformer_out)
 
-        return output
+        return out
+
+    def get_tgt_mask(self, size) -> torch.tensor:
+        # Todo: Check if this is suitable for 3D as example is 2D
+        # Generates a square matrix where the each row allows one word more to be seen
+        mask = torch.tril(torch.ones(size, size) == 1)  # Lower triangular matrix
+        mask = mask.float()
+        mask = mask.masked_fill(mask == 0, float('-inf'))  # Convert zeros to -inf
+        mask = mask.masked_fill(mask == 1, float(0.0))  # Convert ones to 0
+
+        # EX for size=5:
+        # [[0., -inf, -inf, -inf, -inf],
+        #  [0.,   0., -inf, -inf, -inf],
+        #  [0.,   0.,   0., -inf, -inf],
+        #  [0.,   0.,   0.,   0., -inf],
+        #  [0.,   0.,   0.,   0.,   0.]]
+
+        return mask
+
+    def create_pad_mask(self, matrix: torch.tensor, pad_value: int) -> torch.tensor:
+        # Todo: Check if this is needed, as padding is already set to float(-inf)
+        # If matrix = [1,2,3,0,0,0] where pad_value=0, the result mask is
+        # [False, False, False, True, True, True]
+        return matrix == pad_value
