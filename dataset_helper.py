a2af2b741cdeea51065606540add64a0c076d422
Cornelius <cor.reb@posteo.de>
2024-01-22 16:35:50 +0100

    sequence generation moved

--- dataset_helper.py
+++ dataset_helper.py
@@ -1,10 +1,104 @@
-
-
 import numpy as np
+import pandas as pd
+import torch
+
+
+# SEQUENCE GENERATION
+PADDING_VALUE = float('-inf')
+
+
+def generate_input_sequence(logo_embeddings: pd.DataFrame, null_features: int, sequence_length: int,
+                            is_randomized: bool, is_padding: bool) -> torch.Tensor:
+    """
+    Build a torch tensor for the transformer input sequences.
+    Includes
+    - Randomization (optional)
+    - Generation of padding
+
+    Args:
+        logo_embeddings (pd.DataFrame): DataFrame containing logo embeddings.
+        null_features (int): Number of null features to add to each embedding.
+        sequence_length (int): Target length for padding sequences.
+        is_randomized: shuffle order of paths
+        is_padding: if true, function adds padding
+
+    Returns:
+        torch.Tensor: Tensor representing the input sequences.
+    """
+    logo_embeddings.drop(columns=['filename', 'animation_id'], inplace=True)
+
+    # Randomization
+    if is_randomized:
+        logo_embeddings = logo_embeddings.sample(frac=1).reset_index(drop=True)
+
+    # Null Features
+    if null_features > 0:
+        logo_embeddings = pd.concat([logo_embeddings,
+                                     pd.DataFrame(0,
+                                                  index=logo_embeddings.index,
+                                                  columns=range(logo_embeddings.shape[1],
+                                                                logo_embeddings.shape[1] + null_features))],
+                                    axis=1,
+                                    ignore_index=True)
+
+    if is_padding:
+        logo_embeddings = _add_padding(logo_embeddings, sequence_length)
+
+    return torch.tensor(logo_embeddings.values)
+
+
+def generate_output_sequence(animation: pd.DataFrame, sequence_length: int, is_randomized: bool,
+                             is_padding: bool) -> torch.Tensor:
+    """
+    Build a torch tensor for the transformer output sequences.
+    Includes
+    - Randomization (later, when same start time)
+    - Generation of padding
+    - Add EOS Token
+
+    Args:
+        animation (pd.DataFrame): DataFrame containing logo embeddings.
+        sequence_length (int): Target length for padding sequences.
+        is_randomized: shuffle order of paths, applies when same start time
+        is_padding: if true, function adds padding
+
+    Returns:
+        torch.Tensor: Tensor representing the input sequences.
+    """
+    if is_randomized:
+        animation = animation.sample(frac=1).reset_index(drop=True)
+        print("Note: Randomization not implemented yet")
+
+    animation.sort_values(by=['a13'], inplace=True)  # again ordered by time start.
+    animation.drop(columns=['file', 'filename'], inplace=True)
+
+    # Append the EOS row to the DataFrame
+    sos_eos_row = {col: 0 for col in animation.columns}
+    sos_eos_row["a0"] = 1
+    sos_eos_row = pd.DataFrame([sos_eos_row])
+    animation = pd.concat([sos_eos_row, animation, sos_eos_row],
+                          ignore_index=True)
+
+    # Padding Generation: Add padding rows or cut off excess rows
+    if is_padding:
+        animation = _add_padding(animation, sequence_length)
+
+    return torch.Tensor(animation.values)
+
+
+def _add_padding(dataframe: pd.DataFrame, sequence_length: int) -> pd.DataFrame:
+    if len(dataframe) < sequence_length:
+        padding_rows = pd.DataFrame([[PADDING_VALUE] * len(dataframe.columns)] * (sequence_length - len(dataframe)),
+                                    columns=dataframe.columns)
+        dataframe = pd.concat([dataframe, padding_rows], ignore_index=True)
+    elif len(dataframe) > sequence_length:
+        # Cut off excess rows
+        dataframe = dataframe.iloc[:sequence_length]
+
+    return dataframe
 
 
 # BUCKETING
-
 def generate_buckets_2D(dataset, column1, column2, quantiles1, quantiles2, print_histogram=True):
     """
 
